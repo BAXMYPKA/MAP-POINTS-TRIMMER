325aca48af56ea0106bcde26a0418b172711c20e
BAXMYPKA <mrbaxmypka@gmail.com>
2020-04-18 11:01:25 +0300

    Spring project created

---
+++ src/main/java/mrbaxmypka/gmail/com/LosusPOIconverter/LosusPoiConverterApplication.java
@@ -0,0 +1,13 @@
+package mrbaxmypka.gmail.com.LosusPOIconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LosusPoiConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LosusPoiConverterApplication.class, args);
+	}
+
+}
