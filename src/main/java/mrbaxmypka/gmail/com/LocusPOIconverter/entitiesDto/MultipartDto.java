3fd707b486fdd20fabb5104bda417269142ecf4e
BAXMYPKA <mrbaxmypka@gmail.com>
2020-05-01 10:53:50 +0300

    Xml and Html handlers are separeted

--- src/main/java/mrbaxmypka/gmail/com/LocusPOIconverter/entitiesDto/MultipartDto.java
+++ src/main/java/mrbaxmypka/gmail/com/LocusPOIconverter/entitiesDto/MultipartDto.java
@@ -1,17 +1,21 @@
 package mrbaxmypka.gmail.com.LocusPOIconverter.entitiesDto;
 
 import lombok.*;
+import org.springframework.lang.Nullable;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.constraints.NotNull;
+import java.io.Serializable;
 
 @AllArgsConstructor
 @RequiredArgsConstructor
 @EqualsAndHashCode(of = {"multipartFile"})
 @Getter
-public class MultipartDto {
+public class MultipartDto implements Serializable {
 	
-	@NonNull //Lombok
+	static final long serialVersionUID = 3L;
+	
+	@NonNull //Lombok required arg for the constructor
 	@NotNull
 	private MultipartFile multipartFile;
 	
@@ -32,11 +36,24 @@ public class MultipartDto {
 	
 	private boolean validateXml;
 	
+	/**
+	 * Enables or disables using of {@link #path}
+	 */
 	private boolean setPath;
-	
+	/**
+	 * Local or Http path to all the attachments inside HTML descriptions
+	 */
+	@Nullable
 	private String path;
 	
+	/**
+	 * Enables or disables using {@link #previewSize}
+	 */
 	private boolean setPreviewSize;
 	
+	/**
+	 * Attached photos preview size (in width) in pixels
+	 */
+	@Nullable
 	private Integer previewSize;
 }
--- src/main/java/mrbaxmypka/gmail/com/LocusPOIconverter/klm/HtmlHandler.java
+++ src/main/java/mrbaxmypka/gmail/com/LocusPOIconverter/klm/HtmlHandler.java
@@ -5,109 +5,47 @@ import mrbaxmypka.gmail.com.LocusPOIconverter.entitiesDto.MultipartDto;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
 
-import javax.xml.XMLConstants;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.stream.*;
-import javax.xml.stream.events.Characters;
-import javax.xml.stream.events.XMLEvent;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.validation.Schema;
-import javax.xml.validation.SchemaFactory;
-import javax.xml.validation.Validator;
-import java.io.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
 /**
- * Kml processing class based on the StAX xml-library.
+ * HTML processing class based on {@link org.jsoup.Jsoup} library to parse CDATA as HTML.
  */
 @NoArgsConstructor
 @Component
-public class XmlHandler {
-	
-	private XMLEventFactory eventFactory;
-	private XMLEventWriter eventWriter;
-	
-	@Autowired
-	private HtmlHandler htmlHandler;
+public class HtmlHandler {
 	
 	/**
-	 * All the additional information for a User (preview size, outdated descriptions etc) are placed inside the
-	 * CDATA[[]] as an HTML markup.
-	 * So the main goal for this method is the extracting CDATA and pass it to the HTML parser.
+	 * @param htmlCdata Only {@link String} with inner HTML markup
+	 * @param multipartDto To determine all other conditions to be processed on CDATA HTML
+	 * @return Fully processed HTML markup to be included in CDATA block.
 	 */
-	public String processKml(MultipartDto multipartDto)
-		throws XMLStreamException, IOException, ParserConfigurationException, SAXException, TransformerException {
+	public String processCdata(String htmlCdata, MultipartDto multipartDto) {
 		
-		StringWriter stringWriter = new StringWriter();
+		//TODO: to start and end with <!-- desc_gen:start -->
 		
-		XMLInputFactory inputFactory = XMLInputFactory.newInstance();
-		XMLEventReader eventReader = inputFactory.createXMLEventReader(multipartDto.getMultipartFile().getInputStream());
+		Element parsedHtmlFragment = Jsoup.parseBodyFragment(htmlCdata).body();
 		
-		XMLOutputFactory outputFactory = XMLOutputFactory.newInstance();
-		eventFactory = XMLEventFactory.newInstance();
-		eventWriter = outputFactory.createXMLEventWriter(stringWriter);
-		
-		if (multipartDto.isValidateXml()) {
-			Document document = getDocument(multipartDto.getMultipartFile().getInputStream());
-			validateXml(document);
+		if (multipartDto.isClearDescriptions()) { //Should be the first treatment
+			clearDescriptions(parsedHtmlFragment);
 		}
-		//To skip the whole processing if nothing is set
-		if (!multipartDto.isSetPath() &&
-			!multipartDto.isTrimDescriptions() &&
-			!multipartDto.isSetPreviewSize() &&
-			!multipartDto.isClearDescriptions() &&
-			!multipartDto.isTrimXml()) {
-			return new String(multipartDto.getMultipartFile().getBytes());
+		if (multipartDto.isSetPath()) {
+			setPath(parsedHtmlFragment, multipartDto.getPath());
 		}
-		
-		while (eventReader.hasNext()) {
-			XMLEvent event = eventReader.nextEvent();
-			switch (event.getEventType()) {
-				case XMLEvent.CHARACTERS:
-				case XMLEvent.CDATA:
-					event = processCdata(event.asCharacters(), multipartDto);
-			}
-			writeXmlEvent(event);
+		if (multipartDto.isSetPreviewSize()) {
+			Integer previewSize = multipartDto.getPreviewSize() == null ? 0 : multipartDto.getPreviewSize();
+			setPreviewSize(parsedHtmlFragment, previewSize);
 		}
-		System.out.println("\n================ THE RESULT =================================\n");
-		System.out.println(stringWriter);//TODO: to delete one
-		return stringWriter.toString();
+		if (multipartDto.isTrimDescriptions()) { // MUST be the last treatment in all the conditions
+			parsedHtmlFragment.html(trimDescriptions(parsedHtmlFragment));
+		}
+		return parsedHtmlFragment.html();
 	}
 	
-	private Document getDocument(InputStream kmlInputStream)
-		throws ParserConfigurationException, IOException, SAXException {
-		DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-		Document kmlDocument = documentBuilder.parse(kmlInputStream);
-		return kmlDocument;
-	}
 	
-	private void writeXmlEvent(XMLEvent event) throws XMLStreamException {
-		eventWriter.add(event);
-	}
-	
-	private XMLEvent processCdata(Characters characters, MultipartDto multipartDto) {
-		
-		if (characters.isWhiteSpace() || !characters.getData().startsWith("<!-- desc_gen:start -->")) {
-			return eventFactory.createCharacters(characters.getData());
-		}
-		//Obtain an inner CDATA text to treat as HTML elements
-		String processedHtmlCdata = htmlHandler.processCdata(characters.getData(), multipartDto);
-		return eventFactory.createCData(processedHtmlCdata);
-	}
-	
-	//TODO: To treat LocusPOI Images started with 'file://'
-	
-/*
 	private void setPath(Element parsedHtmlFragment, String path) {
 		Elements aElements = parsedHtmlFragment.select("a[href]");
 		Elements imgElements = parsedHtmlFragment.select("img[src]");
@@ -127,9 +65,7 @@ public class XmlHandler {
 				img.attr("src", newPathWithFilename);
 			});
 	}
-*/
-
-/*
+	
 	private String getNewHrefWithOldFilename(String oldHrefWithFilename, String newHrefWithoutFilename) {
 //		Each existing a[href] contains a full path with the filename as the last text element.
 //		Here we have to replace only the URL and leave the original filename.
@@ -179,9 +115,7 @@ public class XmlHandler {
 		
 		parsedHtmlFragment.html(newHtmlDescription.html());
 	}
-*/
-
-/*
+	
 	private Elements createNewHtmlDescription() {
 		Element font = new Element("font");
 		font.attr("color", "black");
@@ -195,14 +129,12 @@ public class XmlHandler {
 		return new Elements(font);
 	}
 	
-	*/
-/**
+	/**
 	 * Filters td Elements with DateTime, gets the earliest, gets its Parent Node with all the table rows which contain
 	 * the whole description of a POI.
 	 *
 	 * @return {@code new Elements("<tr>")} with the whole POI description for the earliest DateTime.
-	 *//*
-
+	 */
 	private Elements getTableRowsWithDescAndDateTime(Elements htmlElements) {
 		Elements tdElementsWithDescription = htmlElements.select("td[align],[valign]");
 		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
@@ -240,13 +172,5 @@ public class XmlHandler {
 		tr.appendChild(td);
 		return tr; //Returns just <tr> with <td> with <hr> inside as a table rows separator
 	}
-*/
 	
-	private void validateXml(Document document) throws SAXException, IOException {
-		SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-		Schema schema = schemaFactory.newSchema(
-			new File("src/main/resources/static/xsd/kml-2.2.0/ogckml22.xsd"));
-		Validator validator = schema.newValidator();
-		validator.validate(new DOMSource(document));
-	}
 }
